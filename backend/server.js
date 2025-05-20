15944a112938d29308f16b19d76210769fd75d94
AjinkyaLY <ajinkya.b@ly.design>
2025-05-20 17:59:10 +0530

    changes for backend

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,7 @@
 require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
+const path = require("path");
 
 const connectDB = require("./config/db");
 
@@ -16,6 +17,19 @@ app.use("/api/questions", require("./routes/questions"));
 app.use("/api/responses", require("./routes/responses"));
 app.use("/api/admin", require("./routes/admin"));
 
+// For production
+if (process.env.NODE_ENV?.trim().toLowerCase() === "production") {
+  app.use(express.static(path.join(__dirname, "../client/dist")));
+
+  app.get("*", (req, res) => {
+    try {
+      res.sendFile(path.resolve(__dirname, "../client/dist/index.html"));
+    } catch (error) {
+      throw new Error(error);
+    }
+  });
+}
+
 const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, () => console.log(`Server running on ${PORT}`));
+app.listen(PORT, () => console.log(`Server running on http://localhost:${PORT}`));
